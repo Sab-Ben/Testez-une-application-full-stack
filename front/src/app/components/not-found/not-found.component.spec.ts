7aa824443347c263623c58d2ce85792ff8f2acb1
Sab-Ben <sbenseghir.sb@gmail.com>
2023-08-23 10:04:49 +0200

    Test page not-found

--- front/src/app/components/not-found/not-found.component.spec.ts
+++ front/src/app/components/not-found/not-found.component.spec.ts
@@ -21,4 +21,11 @@ describe('NotFoundComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have an h1 tag', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain(
+        'Page not found !'
+    );
+  });
 });
